707c0b537be14b48b1f705b9648cbda79be5e2b9
Utsav Barnwal <u.barnwal36@gmail.com>
2022-04-10 20:13:00 +0530

    update package for main app (#24)

--- Connection/src/main/java/com/isolpro/library/connection/helpers/Utils.kt
+++ Connection/src/main/java/com/isolpro/library/connection/helpers/Utils.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import com.google.gson.JsonObject
 import java.io.*
 
 object Utils {
